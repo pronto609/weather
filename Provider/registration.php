f0acc311fc3c59ec34c17c00f0a0acb8863e1ae4
Dev Dev609 <ddevdev609@gmail.com>
2022-05-26 15:14:45 +0300

    Weather Pdovider

--- Provider/registration.php
+++
@@ -1,5 +0,0 @@
-<?php
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Weather_Provider', __DIR__);
--- README.md
+++
@@ -1,1 +0,0 @@
-#bin/magento module:enable Weather_Provider #bin/magento setup:upgrade
